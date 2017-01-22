f45f32ea451f8f12f68bc3f6f60d4b034a1c0409
phineas <phin@phineas.io>
2017-01-22 17:16:29 +0000

    Directory format & basic file base setup

---
+++ config/main.js
@@ -0,0 +1,6 @@
+module.exports = {
+  enabled:{
+    'twitter': false,
+    'github': false
+  }
+};
---
+++ config/social.js
@@ -0,0 +1,10 @@
+module.exports = {
+  twitter:{
+    'twitter_consumer': '',
+    'twitter_consumer_secret': ''
+  },
+  github:{
+    'github_user': '',
+    'github_pass': ''
+  }
+}
