2459824f81e52341dd65b5c43e6a384c1085f2b7
hugmanrique <hugmanrique@gmail.com>
2017-01-27 17:59:09 -0500

    Implemented discord

---
+++ source/lib/services/new/discord.js
@@ -0,0 +1,34 @@
+import {Service} from '../service';
+
+const baseUrl = 'https://discordapp.com/api/';
+
+class Discord extends Service {
+    buildRequest(builder) {
+        let config = this.config;
+
+        this.request = builder({
+            oauth: {
+                consumer_key: config.consumerKey,
+                consumer_secret: config.consumerSecret,
+                token: config.accessTokenKey,
+                token_secret: config.accessTokenSecret
+            }
+        });
+    }
+
+    process() {
+        let image = this.profile.image;
+        
+        if (!image) {
+            return this._callback('No image provided!');
+        }
+
+        this.request.patch({
+            url: baseUrl + 'users/{@me}'
+        }, {
+            avatar: image
+        }, super._handleRes);
+    }
+}
+
+export {Discord as Class};
